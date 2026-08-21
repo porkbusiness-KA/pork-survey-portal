5a86c2650ca3e694628c52cc0c1f415d1e020879
porkbusiness-KA <porkbusiness.ka@gmail.com>
2026-08-21 17:55:20 +0530

    Remove None option from Question 23 Weekly Volume and Product Varieties dropdowns

--- frontend/src/data/surveyQuestions.js
+++ frontend/src/data/surveyQuestions.js
@@ -72,7 +72,7 @@ export const MEAT_TYPES_OPTIONS = [
   { id: 'Other', en: 'Other', kn: 'ಇತರ' }
 ];
 
-export const PROCESSED_VOLUME_OPTIONS = ['<1 Kg', '<3 Kg', '<5 Kg', '>5 Kg', '>10 Kg', 'None', 'Other'];
+export const PROCESSED_VOLUME_OPTIONS = ['<1 Kg', '<3 Kg', '<5 Kg', '>5 Kg', '>10 Kg', 'Other'];
 
 export const PROCESSED_PRODUCT_TYPES = [
   'Ham',
@@ -82,8 +82,7 @@ export const PROCESSED_PRODUCT_TYPES = [
   'Sausage',
   'Ham & Bacon',
   'All Varieties (Ham, Bacon, Salami, Pepperoni, Sausage)',
-  'Others',
-  'None'
+  'Others'
 ];
 
 export const CUSTOMER_TYPE_OPTIONS = [
